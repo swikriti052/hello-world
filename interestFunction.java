6afe9f4cbe9aacc6f947cd93c30a43962ad07d30
djm101 <djm101@users.noreply.github.com>
2017-03-13 08:43:33 -0400

    Update interestFunction.java

--- interestFunction.java
+++ interestFunction.java
@@ -1,1 +1,15 @@
 //
+public static void main(String []args){
+
+ //Declare variables   
+ double price=50.00;
+ double salestax=0.07;
+ double finalprice;
+
+ //Calculate finalprice
+ finalprice=price+(salestax*price);
+
+System.out.println(finalprice=price+(salestax*price));
+
+}
+}
